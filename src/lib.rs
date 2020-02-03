89fbb2d04fa5e8a681aa43da25957c91546fac25
7db9a <repo.7db9a@gmail.com>
2020-02-03 16:46:12 -0500

    fix: changes to pass test.

--- src/lib.rs
+++ src/lib.rs
@@ -10,19 +10,16 @@ pub fn voteoncreq(
    creq_id: u64,
    voter: AccountName,
 ) {
-    require_auth(voter);
-
     let _self = current_receiver();
-    let table = creq::table(_self, _self);
+    let creq_table = creq::table(_self, _self);
+    let vote_table = vote::table(_self, _self);
 
-    table
+    creq_table
         .find(creq_id)
         .is_some()
         .check("change request doesn't exist");
 
-    let table = vote::table(_self, _self);
-
-    let vote_id = table.available_primary_key().expect("failed to get primary key");
+    let vote_id = vote_table.available_primary_key().expect("failed to get primary key");
 
     let a_vote = vote {
         vote_id,
@@ -31,8 +28,10 @@ pub fn voteoncreq(
         vote: 1,
     };
 
-    table.emplace(voter, &a_vote).check("write");
+    vote_table.emplace(voter, &a_vote).check("write");
 }
+
+eosio_cdt::abi!(mkdir, addfile, removefile, sendcreq, voteoncreq);
 
 #[eosio::table("vote")]
 pub struct vote {
