167b33e0e773a95e5190806f5cbaa0f38326c31c
7db9a <repo.7db9a@gmail.com>
2020-02-03 15:53:09 -0500

    Add vote on request.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,28 @@ pub fn voteoncreq(
    creq_id: u64,
    voter: AccountName,
 ) {
+    require_auth(voter);
+
+    let _self = current_receiver();
+    let table = creq::table(_self, _self);
+
+    table
+        .find(creq_id)
+        .is_some()
+        .check("change request doesn't exist");
+
+    let table = vote::table(_self, _self);
+
+    let vote_id = table.available_primary_key().expect("failed to get primary key");
+
+    let a_vote = vote {
+        vote_id,
+        creq_id,
+        voter,
+        vote: 1,
+    };
+
+    table.emplace(voter, &a_vote).check("write");
 }
 
 #[eosio::table("vote")]
