60d73b1ea0eaa5c04516e230e6a2b4e560e14749
7db9a <repo.7db9a@gmail.com>
2020-02-03 15:41:34 -0500

    Added vote struct.

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,12 @@ pub fn voteoncreq(
    voter: AccountName,
 ) {
 }
+
+#[eosio::table("vote")]
+pub struct vote {
+ #[eosio(primary_key)]
+   vote_id: u64,
+   creq_id: u64,
+   voter: AccountName,
+   vote: u64,
+}
