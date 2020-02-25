ec3159c30f28283d7127564a66810cb875cba228
7db9a <repo.7db9a@gmail.com>
2020-02-25 15:35:47 -0500

    tests(vote): fail to assert no vote without tokens.

--- testing/tests/vote.js
+++ testing/tests/vote.js
@@ -318,5 +318,65 @@ describe('Vote', function () {
             assert.equal(vote, 1, "Voted '1' for 'yes'" );
             assert.equal(vote_amount_convert, '20.0000');
         });
+
+        it('Should not allow an account with no tokens to vote on a change request', async () => {
+            var contract = await eoslimeTool.Contract.deployOnAccount(DIR_WASM_PATH, DIR_ABI_PATH, receiverAccount);
+            await contract.mkdir(receiverAccount.name, "dir");
+            let vote_setup_res = await voteSetup(
+                contract,
+                receiverAccount,
+                "src/lib.rs",
+                "QmcDsPV7QZFHKb2DNn8GWsU5dtd8zH5DNRa31geC63ceb1",
+                snooze_ms
+            );
+
+            contract = vote_setup_res[0];
+            let creq_id = vote_setup_res[1];
+
+            await snooze(snooze_ms);
+
+            // Vote
+
+            vote_res = await vote_change_request(contract, creq_id, 0);
+
+            // Second user
+
+            receiverAccount = await createRandomUser('0.0000');
+            contract = await eoslimeTool.Contract.at(contract.name, receiverAccount);
+
+            await snooze(snooze_ms);
+
+            //Voting again is not allowed!
+            try {
+                await vote_change_request(contract, creq_id, 0);
+             } catch (error) {
+                     err_json = JSON.parse(error);
+                     err_code = err_json.code;
+                     if (err_code == 500) {
+                         eosio_err_code = err_json.error.code;
+                         eosio_err_name = err_json.error.name;
+                     }
+             }
+
+            vote = vote_res[0];
+            vote_amount = vote_res[1];
+            vote_creq_id = vote_res[2];
+            vote_count = vote_res[3];
+            warning_tbl_length = vote_res[4];
+
+            let vote_amount_convert = await convert_eos_token_amount(vote_amount);
+
+            assert.equal(vote_count, 1, warning_tbl_length);
+            assert.equal(creq_id, vote_creq_id, "The vote table doesn't have the right change request ID.");
+            assert.equal(vote, 0, "Voted '0' for 'no'" );
+            assert.equal(vote_amount_convert, '20.0000');
+
+            // If we get a 409, it likely means the two contract calls were seen as duplicates
+            // by nodeos. Basically a data race. Consider sleeping for a few ms in between
+            // contract calls. Search the web for "nodejs async sleep".
+            assert.equal(err_code, 409, "Instead of an Internal Appliation Error, we got: " + err_json);
+            assert.equal(eosio_err_code, 3050003, "Duplicate vote.");
+            assert.equal(eosio_err_name, "eosio_assert_message_exception", "Duplicate vote.");
+        });
     });
 });
